f424c08aa6f8bb645d0babcada13a35d0818fe67
ehmatthes <eric@example.com>
2017-01-25 18:40:58 +0800

    Set DEBUG=False for Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -146,7 +146,9 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # 支持所有的主机头（host header)
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['lyz-ll.herokuapp.com']
+
+    DEBUG = False
 
     #静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
